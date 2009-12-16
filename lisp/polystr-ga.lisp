e8fc6e5cf1649da5387e1bcd43b7fd041bea0223
Jesse Alama <alama@stanford.edu>
2009-12-16 18:48:00 +0000

    Fixed cardinality (i.e., fixed V, E, and F) search.

--- lisp/polystr-ga.lisp
+++ lisp/polystr-ga.lisp
@@ -21,6 +21,16 @@
     ; segment (0 1 2) doesn't mean anything
     (random-permutation genome)))
 
+(defun random-fixed-cardinality-poly-str-genome (num-vertices num-edges num-faces)
+  (let ((genome (make-array (list (+ num-vertices num-edges num-faces)))))
+    (dotimes (i num-vertices)
+      (setf (aref genome i) 0))
+    (dotimes (i num-edges)
+      (setf (aref genome (+ num-vertices i)) 1))
+    (dotimes (i num-faces)
+      (setf (aref genome (+ num-vertices num-edges i)) 2))
+    genome))
+
 (defun random-binary-array (dim)
   (let ((m (make-array (list dim dim))))
     (dotimes (i dim m)
@@ -34,6 +44,12 @@
 (defun random-typed-poly-str (total-cardinality)
   (make-poly-str (random-typed-poly-str-genome total-cardinality)
 		 (random-binary-array total-cardinality)))
+
+(defun random-fixed-cardinality-poly-str (num-vertices num-edges num-faces)
+  (make-poly-str (random-fixed-cardinality-poly-str-genome num-vertices
+							   num-edges
+							   num-faces)
+		 (random-binary-array (+ num-vertices num-edges num-faces))))
 
 (defun mutate-poly-str (poly-str)
   (if (zerop (random 2)) ; mutate the type vector
@@ -63,6 +79,21 @@
 	      (setf (aref new-matrix random-x random-y) 1)
 	      (setf (aref new-matrix random-x random-y) 0))
 	  (make-poly-str (type-vector poly-str) new-matrix)))))
+
+(defun mutate-fixed-cardinality-poly-str (poly-str)
+  (let* ((matrix (incidence-matrix poly-str))
+	 (card (cardinality poly-str))
+	 (new-matrix (make-array (list card card))))
+    (dotimes (i card)
+      (dotimes (j card)
+	(setf (aref new-matrix i j)
+	      (aref matrix i j))))
+    (let ((random-x (random card))
+	  (random-y (random card)))
+      (if (zerop (aref matrix random-x random-y))
+	  (setf (aref new-matrix random-x random-y) 1)
+	  (setf (aref new-matrix random-x random-y) 0))
+      (make-poly-str (type-vector poly-str) new-matrix))))
 
 (defun crossover-point (poly-str-1 poly-str-2)
   (declare (ignore poly-str-2))
@@ -108,6 +139,15 @@
 	(let ((m (meld-matrices matrix-1 matrix-2 cross-x cross-y)))
 	  (make-poly-str new-genome m))))))
 
+(defun combine-fixed-cardinality-poly-str (poly-str-1 poly-str-2)
+  (let ((matrix-1 (incidence-matrix poly-str-1))
+	(matrix-2 (incidence-matrix poly-str-2))
+	(genome (type-vector poly-str-1)))
+    (destructuring-bind (cross-x . cross-y)
+	(crossover-point matrix-1 matrix-2)
+      (let ((m (meld-matrices matrix-1 matrix-2 cross-x cross-y)))
+	(make-poly-str genome m)))))
+
 (defun poly-str-fitness (poly-str)
   (let ((n 0))
     (dolist (ax steinitz-rademacher-axioms n)
@@ -140,5 +180,40 @@
 				 #'typed-poly-str-fitness
 				 #'mutate-poly-str
 				 #'combine-poly-str)))))
+
+(defun typed-simplicial-poly-str-ga (population-size num-generations cardinality)
+  (let ((pop (random-population population-size
+				#'(lambda ()
+				    (random-poly-str cardinality)))))
+    (dotimes (i num-generations pop)
+      (format t "Generation ~S~%" i)
+      (setf pop (next-generation pop 
+				 #'typed-poly-str-simplicial-fitness
+				 #'mutate-poly-str
+				 #'combine-poly-str)))))
+
+(defun typed-poly-str-weighted-ga (population-size num-generations cardinality)
+  (let ((pop (random-population population-size
+				#'(lambda ()
+				    (random-typed-poly-str cardinality)))))
+    (dotimes (i num-generations pop)
+      (format t "Generation ~S~%" i)
+      (setf pop (next-generation pop 
+				 #'poly-str-weighted-fitness
+				 #'mutate-poly-str
+				 #'combine-poly-str)))))
+
+(defun typed-fixed-cardinality-ga (num-vertices num-edges num-faces
+				   population-size num-generations)
+  (let ((pop (random-population population-size
+				#'(lambda ()
+				    (random-fixed-cardinality-poly-str
+				     num-vertices num-edges num-faces)))))
+    (dotimes (i num-generations pop)
+      (format t "Generation ~S~%" i)
+      (setf pop (next-generation pop 
+				 #'poly-str-weighted-fitness
+				 #'mutate-fixed-cardinality-poly-str
+				 #'combine-fixed-cardinality-poly-str)))))
 
 ;;; polystr-ga.lisp ends here
