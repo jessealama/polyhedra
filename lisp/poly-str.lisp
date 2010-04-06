a034b4f1a11b3ef2d93620830e5759c5c4e9c3d9
Jesse Alama <alama@stanford.edu>
2010-04-06 22:12:46 +0100

    Move formula-specific code to formulas.lisp.

--- lisp/poly-str.lisp
+++ lisp/poly-str.lisp
@@ -1,91 +1,7 @@
 ;;; Polyhedra as incidence structures
 
-(defun top? (formula)
-  (eq (car formula) 'top))
-
-(defun bottom? (formula)
-  (eq (car formula) 'bottom))
-
-(defun negation? (formula)
-  (eq (car formula) 'not))
-
-(defun unnegate (negation)
-  (second negation))
-
-(defun implication? (formula)
-  (eq (car formula) 'implies))
-
-(defun antecedent (implication)
-  (cadr implication))
-
-(defun consequent (implication)
-  (caddr implication))
-
-(defun equivalence? (formula)
-  (eq (car formula) 'iff))
-
-(defun lhs (formula)
-  (cadr formula))
-
-(defun rhs (formula)
-  (caddr formula))
-
-(defun disjunction? (formula)
-  (eq (car formula) 'or))
-
-(defun disjuncts (disjunction)
-  (cdr disjunction))
-
-(defun conjunction? (formula)
-  (eq (car formula) 'and))
-
-(defun conjuncts (conjunction)
-  (cdr conjunction))
-
-(defun universal? (formula)
-  (eq (car formula) 'all))
-
-(defun existential? (formula)
-  (eq (car formula) 'exists))
-
-(defun matrix (quantified-formula)
-  (caddr quantified-formula))
-
-(defun bound-variable (quantified-formula)
-  (cadr quantified-formula))
-
-(defun bare-variable? (variable)
-  (symbolp variable))
-
-(defun typed-variable? (variable)
-  (not (bare-variable? variable)))
-
-(defun variable-type (typed-variable)
-  (second typed-variable))
-
-(defun variable-name (typed-variable)
-  (first typed-variable))
-
-(defun v-statement? (formula)
-  (eq (car formula) 'V))
-
-(defun e-statement? (formula)
-  (eq (car formula) 'E))
-
-(defun f-statement? (formula)
-  (eq (car formula) 'F))
-
-(defun equation? (formula)
-  (eq (car formula) '=))
-
-(defun unary-statement-argument (unary-statement)
-  (cadr unary-statement))
-
-(defun binary-statement-first-arg (binary-statement)
-  (cadr binary-statement))
-
-(defun binary-statement-second-arg (binary-statement)
-  (caddr binary-statement))
+(require 'utils "utils")
+(require 'formulas "formulas")
 
 ;;; Structures
 
@@ -456,5 +372,16 @@ according to the typed presentation of the axioms."
 	    (1 1 0 0 1 1)
 	    (1 1 1 1 0 0)
 	    (1 1 1 1 0 0))))
+
+;;; Successor function
+
+(defun neighbors (poly-str)
+  (let ((card (cardinality poly-str))
+	(matrix (incidence-matrix poly-str))
+	(nbrs nil))
+    (do-square x y card
+      (let ((m (copy-square-matrix matrix)))
+	(push (make-poly-str (type-vector poly-str) (invert m x y)) nbrs)))
+    nbrs))
 
 (provide 'poly-str)
